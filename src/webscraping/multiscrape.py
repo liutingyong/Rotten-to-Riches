3001b09c5ccf55e8b5a2d413b46b7985e44a7540
Ting-Yong Liu <tingyongliu25@gmail.com>
2025-06-30 14:00:47 -0700

    added asyncio example + multiscrape for multiple pages of scraping at once

---
+++ src/webscraping/multiscrape.py
@@ -0,0 +1,4 @@
+import asyncio
+from playwright.async_api import async_playwright
+import re
+# combining webscrape.py and practice_async.py, multiple webscraping takes long, so we use async to speed it up 
